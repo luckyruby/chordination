099751d6d205c0d3bb5ec07d7ff3b3229c1c5efb
Kenneth Lin <klin@luckyruby.com>
2013-06-13 18:32:35 -0400

    refactored entries hash to be a nested hash by bet id added differentials and winners to entry page validating numericality of entry values

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -23,15 +23,45 @@ class EntriesController < ApplicationController
       @bets = @participant.scoresheet.bets.order("position")
       @participants = @participant.scoresheet.participants.accepted.order("position")
       @entries = {}
+      @differentials = {}
+      @winners = {}
+      
+      #initialize hashes by bet id
+      @bets.each do |b|
+        @entries[b.id] = {}
+        @differentials[b.id] = {}
+        @winners[b.id] = {}
+      end
+      
+      #populate entries and differentials hash
       @participants.each do |p|
         p.entries.each do |e|
+          bet_id = e.bet.id
+          entry = e.value.to_i
+          result = e.bet.value.to_i if e.bet.value.present?
+          
           if e.bet.bet_type == 'winner'
-            @entries["#{e.bet.id}:#{p.id}"] = "#{e.winner} by #{e.value}"
+            @entries[bet_id][p.id] = "#{e.winner} by #{e.value}"
+            if result
+              @differentials[e.bet.id][p.id] = if e.bet.winner == e.winner
+                                      (result - entry).abs
+                                    else
+                                      entry + result
+                                    end
+            end
           else
-            @entries["#{e.bet.id}:#{p.id}"] = e.value
+            @entries[bet_id][p.id] = e.value
+            @differentials[e.bet.id][p.id] = (result - entry).abs if result
           end
         end
       end
+      
+      #determine winners of each bet
+      @bets.each do |b|
+        lowest_diff = @differentials[b.id].values.sort.first
+        @winners[b.id] = @differentials[b.id].select {|k,v| v == lowest_diff}.keys
+      end
+      
     else
       redirect_to "/entries/#{@participant.key}/new"
     end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -5,5 +5,6 @@ class Entry < ActiveRecord::Base
   attr_accessible :participant_id, :value, :bet_id, :winner
   
   validates_presence_of :value
+  validates_numericality_of :value, :only_integer => true, :greater_than_or_equal_to => 0
   validates_uniqueness_of :bet_id, :scope => :participant_id
 end
