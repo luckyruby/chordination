78a8c4db2e8f3867c8787a69274c35eb2f374088
Kenneth Lin <klin@luckyruby.com>
2013-06-14 05:33:15 -0400

    colorized standings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,5 +26,18 @@ module ApplicationHelper
     options[:ampm] ? object.strftime("%a %m/%d/%Y %I:%M %p %Z") : object.strftime("%m/%d/%Y %H:%M:%S")
   end
   
+  def colorize_points(points)
+    if points < 0
+      content_tag(:span, prettify_points(points), class: 'red')
+    elsif points > 0
+      content_tag(:span, prettify_points(points), class: 'green')      
+    else
+      prettify_points(points)
+    end
+  end
+  
+  def prettify_points(points)
+    sprintf('%g', points)
+  end
     
 end
