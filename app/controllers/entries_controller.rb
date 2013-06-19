c937ead5707bc36f7bd5d0e7fdbec6243b80f2c0
Kenneth Lin <klin@luckyruby.com>
2013-06-19 17:51:43 -0400

    refactored score calculations into scoresheet model out of entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -24,109 +24,15 @@ class EntriesController < ApplicationController
       @bets = @scoresheet.bets.order("position")
       @participants = @scoresheet.participants.accepted.order("position")
       @message = Message.new
-      @entries = {}
-      @differentials = {}
-      @winners = {}
-      @standings = {}
       @results = @scoresheet.results_in?
       
-      #initialize hashes by bet id
-      @bets.each do |b|
-        @entries[b.id] = {}
-        
-        if @scoresheet.expired? && @results
-          @differentials[b.id] = {}
-          @winners[b.id] = {}
-        end
-      end
-      
-      #populate entries and differentials hash
-      @participants.each do |p|
-        p.entries.each do |e|
-          bet_id = e.bet.id
-          entry = e.value.to_i
-          result = e.bet.value.to_i if e.bet.value.present?
-          
-          if e.bet.bet_type == 'winner'
-            @entries[bet_id][p.name] = "#{e.winner} by #{e.value}"
-            if result
-              @differentials[e.bet.id][p.name] = if e.bet.winner == e.winner
-                                      (result - entry).abs
-                                    else
-                                      entry + result
-                                    end
-            end
-          else
-            @entries[bet_id][p.name] = e.value
-            @differentials[e.bet.id][p.name] = (result - entry).abs if result
-          end
-        end
-      end
-      
-      #add results to entries hash
-      if @results
-        @bets.each do |b|
-          @entries[b.id]['result'] = if b.bet_type == "winner"
-                                        "#{b.winner} by #{b.value}"
-                                      else
-                                        b.value
-                                      end
-        end
-      end
-      
+      @entries = @scoresheet.compile_entries(@results)      
       if @scoresheet.expired? && @results
-        
-        #calculate consolation differential
-        if @scoresheet.consolation?
-          @consolation = {}
-          @participants.each do |p|
-            @consolation[p.name] = 0
-            non_winner_bets = @bets.select {|b| b.bet_type != 'winner'}
-            winner_bets = @bets.select{|b| b.bet_type == 'winner'}
-            non_winner_bets.each do |b|
-              @consolation[p.name] += @differentials[b.id][p.name]
-            end
-            winner_bets.each do |b|
-              @consolation[p.name] += @differentials[b.id][p.name]
-              @consolation[p.name] -= 3 if b.winner == p.entries.detect {|e| e.bet_id == b.id}.try(:winner) #3 pt discount if winner picked correctly
-            end
-          end
-        end
-        
-        #determine winners of each bet
-        @bets.each do |b|
-          lowest_diff = @differentials[b.id].values.sort.first
-          @winners[b.id] = @differentials[b.id].select {|k,v| v == lowest_diff}.keys
-        end
-        if @scoresheet.consolation?
-          lowest_consolation = @consolation.values.sort.first
-          @winners['consolation'] = @consolation.select {|k,v| v == lowest_consolation}.keys
-        end
-        
-        #determine standings of each participant
-        participants_count = @participants.length
-        @participants.each do |p|
-          @standings[p.name] = {}
-          @bets.each do |b|
-            winner_count = @winners[b.id].length
-            loser_count = (participants_count - winner_count)
-            @standings[p.name][b.id] =  if @winners[b.id].include?(p.name)
-                                        loser_count * b.points.to_f / winner_count.to_f
-                                      else
-                                        b.points*-1
-                                      end
-          end
-          if @scoresheet.consolation?
-            consolation_winner_count = @winners['consolation'].length
-            consolation_loser_count = (participants_count - consolation_winner_count)        
-            @standings[p.name]['consolation'] = if @winners['consolation'].include?(p.name)
-                                                consolation_loser_count * @scoresheet.consolation_points / consolation_winner_count
-                                              else
-                                                @scoresheet.consolation_points*-1
-                                              end
-          end
-        end
-      end            
+        @differentials = @scoresheet.compile_differentials(@results)
+        @consolation = @scoresheet.compile_consolation(@differentials)
+        @winners = @scoresheet.compile_winners(@differentials, @consolation)
+        @standings = @scoresheet.compile_standings(@winners)
+      end                  
     else
       redirect_to "/entries/#{@participant.key}/new"
     end
--- app/models/scoresheet.rb
+++ app/models/scoresheet.rb
@@ -39,5 +39,112 @@ class Scoresheet < ActiveRecord::Base
       self.participants.build(name: p.name, email: p.email)
     end
   end
+  
+  def compile_entries(results=nil)
+    #initialize hash by bet id
+    entries = {}
+    self.bets.each do |b|
+      entries[b.id] = {}
+      
+      #add results if results are in
+      entries[b.id]['result'] = (b.bet_type == "winner" ? "#{b.winner} by #{b.value}" : b.value) if results
+    end
+    
+    #populate
+    self.participants.accepted.each do |p|
+      p.entries.each do |e|      
+        entries[e.bet_id][p.name] = (e.bet.bet_type == 'winner' ? "#{e.winner} by #{e.value}" : e.value)
+      end
+    end
+    
+    entries    
+  end
+  
+  def compile_differentials(results=nil)
+    #initialize hash by bet_id
+    differentials = {}
+    self.bets.each {|b| differentials[b.id] = {}}
+    
+    #populate
+    self.participants.accepted.each do |p|
+      p.entries.each do |e|
+        entry = e.value.to_i
+        result = e.bet.value.to_i if e.bet.value.present?
+        if result
+          differentials[e.bet_id][p.name] = if e.bet.bet_type == 'winner' && e.bet.winner != e.winner
+            #if you picked the wrong winner
+            entry + result
+          else
+            (result - entry).abs
+          end
+        end
+      end
+    end
+    
+    differentials    
+  end
+  
+  def compile_consolation(differentials={})
+    consolation = {}
+    if self.consolation?
+      self.participants.accepted.each do |p|
+        consolation[p.name] = 0
+        non_winner_bet_types = self.bets.select {|b| b.bet_type != 'winner'}
+        winner_bet_types = self.bets.select{|b| b.bet_type == 'winner'}
+        
+        non_winner_bet_types.each do |b|
+          consolation[p.name] += differentials[b.id][p.name]
+        end
+        
+        winner_bet_types.each do |b|
+          consolation[p.name] += differentials[b.id][p.name]
+          consolation[p.name] -= 3 if b.winner == p.entries.detect {|e| e.bet_id == b.id}.try(:winner) #3 pt discount if winner picked correctly
+        end
+      end
+    end
+    
+    consolation
+  end
+  
+  def compile_winners(differentials={}, consolation={})
+    winners = {}
+    self.bets.each do |b|
+      lowest_diff = differentials[b.id].values.sort.first
+      winners[b.id] = differentials[b.id].select {|k,v| v == lowest_diff}.keys
+    end
+    
+    #add consolation winner
+    if self.consolation? && consolation.present?
+      lowest_consolation = consolation.values.sort.first
+      winners['consolation'] = consolation.select {|k,v| v == lowest_consolation}.keys
+    end
+    
+    winners
+  end
+  
+  def compile_standings(winners)
+    standings = {}
+    participants = self.participants.accepted
+    participants_count = participants.length
+    
+    #populate
+    participants.each do |p|
+      standings[p.name] = {}    
+      self.bets.each do |b|
+        winner_count = winners[b.id].length
+        loser_count = (participants_count - winner_count)
+        standings[p.name][b.id] = (winners[b.id].include?(p.name) ? loser_count * b.points.to_f / winner_count.to_f : b.points*-1)
+      end
+      
+      #add consolation standing
+      if self.consolation?
+        consolation_winner_count = winners['consolation'].length
+        consolation_loser_count = (participants_count - consolation_winner_count)        
+        standings[p.name]['consolation'] = (winners['consolation'].include?(p.name) ? consolation_loser_count * self.consolation_points / consolation_winner_count : self.consolation_points*-1)
+      end
+    end
+    
+    standings
+  end
 
 end
